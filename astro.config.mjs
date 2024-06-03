a3e7d261ab63e244ac4e501eb929cbaf4e6a4b5a
antolinnin00 <perezantolin986@gmail.com>
2024-06-03 15:41:26 +0200

    1.12.6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import auth from "auth-astro";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [auth()],
+  output: "server",
+  adapter: netlify()
+});
